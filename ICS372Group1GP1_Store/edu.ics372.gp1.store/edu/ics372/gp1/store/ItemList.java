7656a12b7a77f3543c92f59c0fd906cfa365bde4
Carl Noren <58144127+carlwnoren@users.noreply.github.com>
2022-03-02 20:26:02 -0600

    Second commit

--- ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/ItemList.java
+++ ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/ItemList.java
@@ -1,5 +1,12 @@
 package edu.ics372.gp1.store;
 
-public interface ItemList {
-
+public interface ItemList <T extends Matchable<K>, K>{
+	public T search(K value) {
+		  for (T element: elements) {
+		    if (element.matches(value)) {
+		      return element;
+		    }
+		  }
+		  return null;
+		}
 }
