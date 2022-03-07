ffaafb4f66778df4204f1a2cff0b09fccab78d5c
JTPham45 <jpham45@ymail.com>
2022-03-06 18:35:30 -0600

    Removed getters and setters not needed

--- ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/Customer.java
+++ ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/Customer.java
@@ -37,30 +37,6 @@ public class Customer {
 		id = CUSTOMER_STRING + idCounter++;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
 	public void addAppliance(Appliance appliance) {
 		appliances.add(appliance);
 	}
