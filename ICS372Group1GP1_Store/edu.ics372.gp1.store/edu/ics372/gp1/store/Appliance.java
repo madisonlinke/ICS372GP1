d0b54b8a77766474ee357b13ae6c60a31c9306b6
Kean Jaycox <kjaycox3030@gmail.com>
2022-03-07 21:38:21 -0600

    Update Appliance.java
    updating appliance

--- ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/Appliance.java
+++ ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/Appliance.java
@@ -1,5 +1,34 @@
+//Kean Jaycox
+//ICS 372 Group Project 1
 package edu.ics372.gp1.store;
 
-public class Appliance {
-    //eclipse is trash sometimes
+public abstract class Appliance implements Matchable<String>{
+    
+    private String brand;
+    private String model;
+    private double cost;
+    private int stock;
+    private String applianceID;
+    
+    @Override
+    public boolean matches(String customerID) {
+        return this.id.equals(customerID);
+    }
+    
+    public Appliance(String brand, String model, double cost, int stock, String applianceID) {
+        this.brand = brand;
+        this.model = model;
+        this.cost = cost;
+        this.stock = stock;
+        this.applianceID = applianceID;
+    }
+    
+    public void addStock() {
+        
+    }
+    
+    public void removeStock() {
+        
+    }
+    
 }
