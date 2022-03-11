be680e4b55e078e4549b06e8856087e4d10c4b30
Carl Noren <58144127+carlwnoren@users.noreply.github.com>
2022-03-10 18:47:50 -0600

    Update Store.java

--- ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/Store.java
+++ ICS372Group1GP1_Store/edu.ics372.gp1.store/edu/ics372/gp1/store/Store.java
@@ -1,5 +1,22 @@
 package edu.ics372.gp1.store;
 
-public class Store {
+import java.io.Serializable;
 
+public class Store implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private Inventory inventory = Inventory.getInstance();
+	private BackorderList backorderList = BackorderList.getInstance();
+	private CustomerList customerList = CustomerList.getInstance();
+	private static Store store;
+	
+	private Store() {
+	}
+	
+	public static Store getInstance() {
+		if (store == null) {
+			return store = new Store();
+		} else {
+			return store;
+		}
+	}
 }
