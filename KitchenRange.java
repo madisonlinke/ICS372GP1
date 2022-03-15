0b5a493975be531dcf6a1688e713e2608538ce0f
madisonlinke <90859673+madisonlinke@users.noreply.github.com>
2022-03-14 21:08:42 -0500

    KitchenRange

---
+++ KitchenRange.java
@@ -0,0 +1,17 @@
+package groupProject;
+
+public class KitchenRange {
+	String brand;
+	String model;
+	double cost;
+	int stock;
+	
+	public KitchenRange(String brand, String model, double cost, int stock) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.cost = cost;
+		this.stock = stock;
+	}
+	
+}
