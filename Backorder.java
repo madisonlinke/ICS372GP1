f3e991c96b525b3624091b0e7c3c2c40c2303ff3
madisonlinke <90859673+madisonlinke@users.noreply.github.com>
2022-03-14 21:09:05 -0500

    Backorder

---
+++ Backorder.java
@@ -0,0 +1,13 @@
+package groupProject;
+
+public class Backorder {
+	String backorderID;
+	Appliance appliance = new Appliance();
+	
+	public Backorder(String backorderID, Appliance appliance) {
+		super();
+		this.backorderID = backorderID;
+		this.appliance = appliance;
+	}
+	
+}
